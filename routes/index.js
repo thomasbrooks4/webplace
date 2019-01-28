c3a93db5fcaae8215a0cec9cdf60f75c651a79b3
Thomas Brooks <brooks.thomas67@gmail.com>
2019-01-27 18:06:45 -0800

    Basic webpage setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Thomas' Webplace" });
+});
+
+router.get('/tychocam/', function(req, res, next) {
+  res.render('tychocam', { title: "Tycho Cam" });
+});
+
+router.get('/whatisthomascurrentlyobsessedwith/', function(req, res, next) {
+  res.render('obsessions', { title: "What is Thomas currently obsessed with?" });
+});
+
+router.get('/neveragain', function(req, res, next) {
+  res.render('neveragain', { title: "Never Again" });
 });
 
 module.exports = router;
